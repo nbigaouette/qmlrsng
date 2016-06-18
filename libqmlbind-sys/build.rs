97610677aec6f605a50f3623cb4b39e557f71383
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-06-17 23:51:38 -0400

    WIP: Add a build script for properly link against libqmlbind and Qt

---
+++ libqmlbind-sys/build.rs
@@ -0,0 +1,40 @@
+use std::path::Path;
+use std::process::Command;
+
+
+fn main() {
+
+    // Initialize git submodule
+    if !Path::new("libqmlbind/.git").exists() {
+        let _ = Command::new("git").args(&["submodule", "update", "--init"])
+                                   .status();
+    }
+
+    Command::new("qmake").current_dir("libqmlbind").output().unwrap();
+    Command::new("make").current_dir("libqmlbind").output().unwrap();
+    Command::new("make").arg("staticlib").current_dir("libqmlbind/qmlbind").output().unwrap();
+
+
+    println!("cargo:rustc-flags=-L libqmlbind/qmlbind");
+    println!("cargo:rustc-link-lib=static=qmlbind");
+    // println!("cargo:rustc-link-lib=qmlbind");
+
+
+    // https://github.com/bitcoin/bitcoin/issues/7714
+    // println!("cargo:rustc-link-lib=Qt");
+    // HACK - missing pkgconfig files in Qt5.6
+    // QT_CFLAGS="-I${QTDIR}/include -I${QTDIR}/include/QtWidgets -I${QTDIR}/include/QtCore \
+    //            -I${QTDIR}/include/QtGui -I${QTDIR}/include/QtNetwork"
+    // QT_LIBS="-F${QTDIR}/lib -framework QtCore -framework QtNetwork -framework QtGui -framework QtWidgets" ./configure --with-gui=qt5
+    // QT_LIBS="-F${QTDIR}/lib -framework QtCore -framework QtNetwork -framework QtGui -framework QtWidgets"
+    let QTDIR = String::from("/Users/nicolas/Qt/5.6/clang_64/lib");
+
+    println!("cargo:rustc-flags=-L framework={}", QTDIR);
+    println!("cargo:rustc-link-lib=framework=QtCore");
+    println!("cargo:rustc-link-lib=framework=QtNetwork");
+    println!("cargo:rustc-link-lib=framework=QtGui");
+    println!("cargo:rustc-link-lib=framework=QtWidgets");
+
+    println!("cargo:rustc-flags=-l stdc++");
+
+}
