dee9a5085ba586d44b8101268fcda216be973603
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-07-02 14:47:06 -0400

    fix_qt_rpath.sh: Loop over all examples

--- libqmlbind-sys/fix_qt_rpath.sh
+++ libqmlbind-sys/fix_qt_rpath.sh
@@ -6,10 +6,14 @@ QTMINVER="${QTMINVER:-7}"
 QTCOMPARCH="${QTCOMPARCH:-clang_64}"
 
 QTDIR="${QTDIR:-${HOME}/Qt/${QTMAJVER}.${QTMINVER}/${QTCOMPARCH}}"
-binary="target/debug/examples/hello_world"
 
 components=(QtCore QtNetwork QtGui QtQml QtWidgets)
 
-for component in ${components[*]}; do
-    install_name_tool -change @rpath/${component}.framework/Versions/${QTMAJVER}/${component} ${QTDIR}/lib/${component}.framework/Versions/${QTMAJVER}/${component} ${binary}
+
+for binary in target/debug/examples/*; do
+    if [[ -f ${binary} ]]; then
+        for component in ${components[*]}; do
+            install_name_tool -change @rpath/${component}.framework/Versions/${QTMAJVER}/${component} ${QTDIR}/lib/${component}.framework/Versions/${QTMAJVER}/${component} ${binary}
+        done
+    fi
 done
