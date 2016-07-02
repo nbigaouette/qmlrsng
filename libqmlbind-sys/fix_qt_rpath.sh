d72aacf77f2ad8e75d7b8555c303560ae3249083
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-07-02 15:31:59 -0400

    Automatically find all examples to patch

--- libqmlbind-sys/fix_qt_rpath.sh
+++ libqmlbind-sys/fix_qt_rpath.sh
@@ -10,10 +10,14 @@ QTDIR="${QTDIR:-${HOME}/Qt/${QTMAJVER}.${QTMINVER}/${QTCOMPARCH}}"
 components=(QtCore QtNetwork QtGui QtQml QtWidgets)
 
 
-for binary in target/debug/examples/*; do
-    if [[ -f ${binary} ]]; then
-        for component in ${components[*]}; do
-            install_name_tool -change @rpath/${component}.framework/Versions/${QTMAJVER}/${component} ${QTDIR}/lib/${component}.framework/Versions/${QTMAJVER}/${component} ${binary}
-        done
-    fi
+for example in examples/*.rs; do
+    binary=`echo ${example} | sed 's|examples/\(.*\).rs|\1|g'`
+    for build in "debug" "release"; do
+        binary="target/${build}/exmaples/${binary}"
+        if [[ -f ${binary} ]]; then
+            for component in ${components[*]}; do
+                install_name_tool -change @rpath/${component}.framework/Versions/${QTMAJVER}/${component} ${QTDIR}/lib/${component}.framework/Versions/${QTMAJVER}/${component} ${binary}
+            done
+        fi
+    done
 done
