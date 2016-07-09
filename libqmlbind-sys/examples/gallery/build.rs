a66be4e7671e26fc0810ee5770f3f81526b9c03d
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-07-08 21:50:14 -0400

    Example 'gallery': Better handling of default QT_DIR in build.rs

--- libqmlbind-sys/examples/gallery/build.rs
+++ libqmlbind-sys/examples/gallery/build.rs
@@ -1,23 +1,43 @@
 use std::path::Path;
+use std::path::PathBuf;
 use std::env;
 use std::process::Command;
 
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    // println!("out_dir: {}", out_dir);
+    let target = env::var("TARGET").unwrap();
 
     // http://doc.crates.io/build-script.html#inputs-to-the-build-script
     // "the build script’s current directory is the source directory of the build script’s package."
     let pwd = env::current_dir().unwrap();
     let src = pwd.join("src");
 
-    // let home_dir = env::home_dir().unwrap();
+    let home_dir = env::home_dir().map(|p| PathBuf::from(p)).unwrap();
 
-    // QT_DIR=${HOME}/Qt/5.7/clang_64
-    let qt_dir = env::var("QT_DIR").unwrap();
+    let qt_dir = env::var("QT_DIR").map(|p| PathBuf::from(p)).unwrap_or({
+        println!("Environnement variable 'QT_DIR' not set!");
+        println!("Defaulting to ${{HOME}}/Qt/${{QT_VER}}/${{QT_COMP}} where:");
+        let default_qt_ver = "5.7".to_string();
+        let default_qt_comp = if target.contains("linux") {
+            "gcc_64".to_string()
+        } else if target.contains("darwin") {
+            "clang_64".to_string()
+        } else {
+            panic!("Unsuported platform in gallery's build.rs!")
+        };
+        println!("    QT_VER:  {}", default_qt_ver);
+        println!("    QT_COMP: {}", default_qt_comp);
+        let qt_dir_default = home_dir.join("Qt")
+                                  .join(env::var("QT_VER").unwrap_or(default_qt_ver))
+                                  .join(env::var("QT_COMP").unwrap_or(default_qt_comp));
+        qt_dir_default
+    });
+    println!("Using Qt directory: {:?}", qt_dir);
+    println!("Use QT_DIR environment variable to ovewrite.");
 
-    let qmake = Path::new(&qt_dir).join("bin").join("qmake");
+    let qmake = qt_dir.join("bin").join("qmake");
+
     // let rcc = Path::new(&qt_dir).join("bin").join("rcc");
     // println!("rcc: {:?}", rcc);
 
