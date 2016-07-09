a7c9f24a52d3a5aa172d5f1f63786caa74341018
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-07-08 22:27:53 -0400

    libqmlbind-sys: Cleanup and fix build.rs

--- libqmlbind-sys/build.rs
+++ libqmlbind-sys/build.rs
@@ -1,8 +1,11 @@
 use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
+use std::env;
 
 
 fn main() {
+    let target = env::var("TARGET").unwrap();
 
     // Initialize git submodule
     if !Path::new("libqmlbind/.git").exists() {
@@ -20,25 +23,40 @@ fn main() {
     // println!("cargo:rustc-link-lib=qmlbind");
 
 
-    // https://github.com/bitcoin/bitcoin/issues/7714
-    // println!("cargo:rustc-link-lib=Qt");
-    // HACK - missing pkgconfig files in Qt5.6
-    // QT_CFLAGS="-I${QTDIR}/include -I${QTDIR}/include/QtWidgets -I${QTDIR}/include/QtCore \
-    //            -I${QTDIR}/include/QtGui -I${QTDIR}/include/QtNetwork"
-    // QT_LIBS="-F${QTDIR}/lib -framework QtCore -framework QtNetwork -framework QtGui -framework QtWidgets" ./configure --with-gui=qt5
-    // QT_LIBS="-F${QTDIR}/lib -framework QtCore -framework QtNetwork -framework QtGui -framework QtWidgets"
-    // let qtdir = String::from("/Users/nicolas/Qt/5.6/clang_64/lib");
-    let qtdir = String::from("/Users/nicolas/Qt/5.7/clang_64/lib");
+    let qt_dir = env::var("QT_DIR").map(|p| PathBuf::from(p)).unwrap_or({
+        println!("Environnement variable 'QT_DIR' not set!");
+        println!("Defaulting to ${{HOME}}/Qt/${{QT_VER}}/${{QT_COMP}} where:");
+        let home_dir = env::home_dir().map(|p| PathBuf::from(p)).unwrap();
+        let default_qt_ver = "5.7".to_string();
+        let default_qt_comp = if target.contains("linux") {
+            "gcc_64".to_string()
+        } else if target.contains("darwin") {
+            "clang_64".to_string()
+        } else {
+            panic!("Unsuported platform in gallery's build.rs!")
+        };
+        println!("    QT_VER:  {}", default_qt_ver);
+        println!("    QT_COMP: {}", default_qt_comp);
+        let qt_dir_default = home_dir.join("Qt")
+                                  .join(env::var("QT_VER").unwrap_or(default_qt_ver))
+                                  .join(env::var("QT_COMP").unwrap_or(default_qt_comp));
+        qt_dir_default
+    });
+    println!("Using Qt directory: {:?}", qt_dir);
+    println!("Use QT_DIR environment variable to ovewrite.");
 
-    println!("cargo:rustc-link-search=framework={}", qtdir);
-    // println!("cargo:rustc-flags=-L framework={}", qtdir);
+    let qt_lib_dir = qt_dir.join("lib").to_str().unwrap().to_string();
 
-    println!("cargo:rustc-link-lib=framework=QtCore");
-    println!("cargo:rustc-link-lib=framework=QtNetwork");
-    println!("cargo:rustc-link-lib=framework=QtGui");
-    println!("cargo:rustc-link-lib=framework=QtQml");
-    println!("cargo:rustc-link-lib=framework=QtWidgets");
+    let osx_framework = if target.contains("darwin") { "=framework" }
+                        else  { "" };
+
+    println!("cargo:rustc-link-search{}={}", osx_framework, qt_lib_dir);
+
+    println!("cargo:rustc-link-lib{}=QtCore", osx_framework);
+    println!("cargo:rustc-link-lib{}=QtNetwork", osx_framework);
+    println!("cargo:rustc-link-lib{}=QtGui", osx_framework);
+    println!("cargo:rustc-link-lib{}=QtQml", osx_framework);
+    println!("cargo:rustc-link-lib{}=QtWidgets", osx_framework);
 
     println!("cargo:rustc-flags=-l stdc++");
-
 }
