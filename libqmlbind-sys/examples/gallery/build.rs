0b53fefc4b42069d533e0d6b0fa38b3b22beffa9
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-07-08 21:54:21 -0400

    Example 'gallery': Cleanup build.rs

--- libqmlbind-sys/examples/gallery/build.rs
+++ libqmlbind-sys/examples/gallery/build.rs
@@ -38,27 +38,15 @@ fn main() {
 
     let qmake = qt_dir.join("bin").join("qmake");
 
-    // let rcc = Path::new(&qt_dir).join("bin").join("rcc");
-    // println!("rcc: {:?}", rcc);
-
-    // let qrc_output = Path::new(&out_dir).join("qrc_gallery.cpp");
-    //
-    // // Run the Qt Resource Compiler (rcc)
-    // // ${QTDIR}/bin/rcc -name gallery examples/gallery.qrc -o target/debug/examples/qrc_gallery.cpp
-    // let output = Command::new(rcc).args(&["-name", "gallery", "src/gallery.qrc",
-    //                                       "-o", qrc_output.to_str().unwrap()])
-    //                               .output()
-    //                               .expect("failed to execute 'rcc' process");
-
     // Run qmake to create a library with the reousrce file
     let output = Command::new(qmake).args(&[src_dir])
                                   .current_dir(&Path::new(&out_dir))
                                   .output()
                                   .expect("failed to execute 'qmake' process");
 
-    // println!("output.status: {}", output.status);
-    // println!("output.stdout: {}", String::from_utf8_lossy(&output.stdout));
-    // println!("output.stderr: {}", String::from_utf8_lossy(&output.stderr));
+    println!("output.status: {}", output.status);
+    println!("output.stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("output.stderr: {}", String::from_utf8_lossy(&output.stderr));
 
     assert!(output.status.success(), "failed to execute qmake process");
 
@@ -75,45 +63,4 @@ fn main() {
 
     println!("cargo:rustc-link-search={}", out_dir);
     println!("cargo:rustc-link-lib=galleryresources");
-
-
-
-
-    // // Initialize git submodule
-    // if !Path::new("libqmlbind/.git").exists() {
-    //     let _ = Command::new("git").args(&["submodule", "update", "--init"])
-    //                                .status();
-    // }
-    //
-    // Command::new("qmake").current_dir("libqmlbind").output().unwrap();
-    // Command::new("make").current_dir("libqmlbind").output().unwrap();
-    // Command::new("make").arg("staticlib").current_dir("libqmlbind/qmlbind").output().unwrap();
-    //
-    //
-    // println!("cargo:rustc-flags=-L libqmlbind/qmlbind");
-    // println!("cargo:rustc-link-lib=static=qmlbind");
-    // // println!("cargo:rustc-link-lib=qmlbind");
-    //
-    //
-    // // https://github.com/bitcoin/bitcoin/issues/7714
-    // // println!("cargo:rustc-link-lib=Qt");
-    // // HACK - missing pkgconfig files in Qt5.6
-    // // QT_CFLAGS="-I${QTDIR}/include -I${QTDIR}/include/QtWidgets -I${QTDIR}/include/QtCore \
-    // //            -I${QTDIR}/include/QtGui -I${QTDIR}/include/QtNetwork"
-    // // QT_LIBS="-F${QTDIR}/lib -framework QtCore -framework QtNetwork -framework QtGui -framework QtWidgets" ./configure --with-gui=qt5
-    // // QT_LIBS="-F${QTDIR}/lib -framework QtCore -framework QtNetwork -framework QtGui -framework QtWidgets"
-    // // let qtdir = String::from("/Users/nicolas/Qt/5.6/clang_64/lib");
-    // let qtdir = String::from("/Users/nicolas/Qt/5.7/clang_64/lib");
-    //
-    // println!("cargo:rustc-link-search=framework={}", qtdir);
-    // // println!("cargo:rustc-flags=-L framework={}", qtdir);
-    //
-    // println!("cargo:rustc-link-lib=framework=QtCore");
-    // println!("cargo:rustc-link-lib=framework=QtNetwork");
-    // println!("cargo:rustc-link-lib=framework=QtGui");
-    // println!("cargo:rustc-link-lib=framework=QtQml");
-    // println!("cargo:rustc-link-lib=framework=QtWidgets");
-    //
-    // println!("cargo:rustc-flags=-l stdc++");
-
 }
