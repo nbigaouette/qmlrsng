2474d274b51be455cee186b23cc433fa983d5cca
Nicolas Bigaouette <nbigaouette@gmail.com>
2016-07-14 23:25:05 -0400

    Add Component struct

---
+++ src/component.rs
@@ -0,0 +1,37 @@
+extern crate libc;
+extern crate libqmlbind_sys as ffi;
+
+use std;
+use engine;
+
+use std::ffi::CString;
+
+
+pub struct Component {
+    component: *mut ffi::qmlbind_component,
+}
+
+impl Drop for Component {
+    fn drop(&mut self) {
+        unsafe {
+            assert!(!self.component.is_null());
+            ffi::qmlbind_component_release(self.component);
+        }
+    }
+}
+
+
+impl Component {
+    // FIXME: Change to Result instead of Option
+    pub fn load_path(engine: &engine::Engine, path: &str) -> Option<Component> {
+        let component = unsafe { ffi::qmlbind_component_new(engine.get_engine()) };
+        if component.is_null() {
+            None
+        } else {
+            // FIXME: Remove unwrap
+            let s = CString::new(path).unwrap();
+            unsafe { ffi::qmlbind_component_load_path(component, s.as_ptr() ); }
+            Some(Component { component: component })
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 
 mod engine;
+mod component;
